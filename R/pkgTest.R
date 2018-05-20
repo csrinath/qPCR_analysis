cdd029cd00b48430f0d15b0c109210ef6f13eca3
Chetan Srinath <chetan.srinath@gmail.com>
2018-05-20 14:18:35 -0700

    Function to install and load packages
    Installs missing packages if needed, and then loads it to library

---
+++ R/pkgTest.R
@@ -0,0 +1,9 @@
+# Loads package "x". If not present in library, installs and then loads "x".
+pkgTest <- function(x)
+{
+  if (!require(x,character.only = TRUE))
+  {
+    install.packages(x,dep=TRUE)
+    if(!require(x,character.only = TRUE)) stop("Package not found")
+  }
+}
