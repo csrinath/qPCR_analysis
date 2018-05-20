2dd7d916dd71e1ec27e630b18ea509ad315a35ec
Chetan Srinath <chetan.srinath@gmail.com>
2018-05-20 16:43:48 -0700

    Updating readExcel
    Functions to read sample info, sample data, and merge data.
    Works for TAR and Results tabs
    Works if there's Ct, RQ, or none

--- R/readExcel.R
+++ R/readExcel.R
@@ -2,6 +2,13 @@ source("R/pkgTest.R")
 pkgTest("tidyverse")
 pkgTest("readxl")
 
+# Makes the filepath "directory/filename". Default directory is data/
+makeFilePath <- function(fileName,directory="data") {
+  filePath <- str_c(directory, fileName, sep="/")
+  return(filePath)
+}
+
+# Reads Sample Info from the "Sample Setup" sheet of data.xls
 qPCR.sample.reader <- function(df, xlsSheet="Sample Setup", skipRows=42) {
   df %>%
     read_excel(sheet = xlsSheet, skip = skipRows) %>%
@@ -10,21 +17,83 @@ qPCR.sample.reader <- function(df, xlsSheet="Sample Setup", skipRows=42) {
     unique()
 }
 
-qPCR.data.reader <- function(df, skipRows=42) {
-  allSheets = excel_sheets(df)
-  xlsSheet = case_when("Technical Analysis Result" %in% allSheets ~ "Technical Analysis Result",
-                       "Results" %in% allSheets ~ "Results", 
-                       TRUE ~ stop('The file must have a sheet labeled "Technical Analysis Result" or "Results"'))
+# Reads Sample Data from the "Technical Analysis Result" or the "Results" sheet of data.xls
+qPCR.data.reader <- function(df) {
   
-  df  %>%
-    read_excel(sheet = xlsSheet, skip = skipRows) %>%
-    select(Sample= "Sample Name", Target="Target Name", Ct = "Ct Mean", DeltaCt.flex="Delta Ct Mean", RQ.flex = "RQ")  %>%
-    filter(startsWith(Sample,"Sample"))
+  # Returns "Technical Analysis Result" or "Results" or "NA" depending on what sheet exists.
+  excelSheet.checker <- function(df){
+    allSheets = excel_sheets(df)
+    xlsSheet = case_when("Technical Analysis Result" %in% allSheets ~ "Technical Analysis Result",
+                         "Results" %in% allSheets ~ "Results", 
+                         TRUE ~ "NA")
+    return(xlsSheet)
+  }
+  
+  # Reads in data from "Technical Analysis Result"
+  dataReader.TAR <- function(df, skipRows=42) {
+    df  %>%
+      read_excel(sheet = "Technical Analysis Result", skip = skipRows)
+  }
+  
+  # Reads in data from "Results"
+  dataReader.Results <- function(df, skipRows=42) {
+    df  %>%
+      read_excel(sheet = "Results", skip = skipRows)
+  }
+  
+  # Parses read-in data and selects appropriate Ct and/or RQ columns
+  dataParser <- function(data) {
+    
+    # Functions to select Ct and/or RQ, apart from Target and Sample info
+    select.Ct.RQ <- function(df) {
+      df %>%
+        select(Sample= "Sample Name", Target="Target Name", Ct = "Ct Mean", RQ.flex = "RQ")
+    }
+    select.Ct <- function(df) {
+      df %>%
+        select(Sample= "Sample Name", Target="Target Name", Ct = "Ct Mean")
+    }
+    select.RQ <- function(df) {
+      df %>%
+        select(Sample= "Sample Name", Target="Target Name", RQ.flex = "RQ")
+    }
+    select.all <- function(df) {
+      df %>%
+        rename(Sample= "Sample Name", Target="Target Name")
+    }
+    
+    # Depending on what columns exist, appropriate data is selected
+    if(all(c("Ct Mean","RQ") %in% colnames(data)))  {
+      return(select.Ct.RQ(data) %>% filter(startsWith(Sample,"Sample")))
+    }
+    if("Ct Mean" %in% colnames(data)) {
+      return(select.Ct(data) %>% filter(startsWith(Sample,"Sample")))
+    }
+    if("RQ" %in% colnames(data)) {
+      return(select.RQ(data) %>% filter(startsWith(Sample,"Sample")))
+    }
+    else {
+      return(select.all(data) %>% filter(startsWith(Sample,"Sample")))
+    }
+  }
+  
+  xlsSheet <- excelSheet.checker(df)
+  if(xlsSheet == "Technical Analysis Result") return(df %>% dataReader.TAR %>% dataParser())
+  if(xlsSheet == "Results") return(df %>% dataReader.Results %>% dataParser())
+  else stop("XLS file does not contain a 'Technical Analysis Result' or 'Results' tab")
+
 }
+
+# Reads Sample Info and Data using the above functions, merges the info, and numbers the replicates
+qPCR.file.reader <- function(df) {
+  samples <- qPCR.sample.reader(df)
+  data <- qPCR.data.reader(df)
   
-#   %>%
-#     inner_join(unique(select(qPCR.sample.reader(df), Sample, Biogroup)),  by = "Sample") %>%
-#     mutate(Replicate = as.integer(str_sub(Sample, start = 8))%%numRep + 1) %>%
-#     arrange(Target, Biogroup, Sample) %>%
-#     filter(Target != "GAPDH")
-# }
+  fullData <- data %>% 
+    inner_join(unique(select(samples, Sample, Target, Biogroup)), by = c("Sample", "Target")) %>%
+    group_by(Biogroup, Target) %>% 
+    mutate(Replicate = row_number()) %>%    # Number the replicates
+    arrange(Target, Biogroup)
+  return(fullData)
+}
+
