66517e79a6a6ef36f737fafa05c36f898098938e
Chetan Srinath <chetan.srinath@gmail.com>
2018-05-20 19:25:55 -0700

    Testing qPCR calculations
    Calculate deltaCt, deltaDeltaCt, and FoldChange

---
+++ test_qPCR_calc.R
@@ -0,0 +1,13 @@
+source("R/readExcel.R")
+source("R/qPCR_calc.R")
+
+# File 1 has Ct and RQ values (based on GAPDH, condition4)
+file <- "data1.xls"
+df <- file %>% makeFilePath() %>% 
+  qPCR.file.reader() %>% 
+  deltaCt("GAPDH") %>%
+  deltaDeltaCt("Condition4") %>%
+  calcRQ()
+
+plot(df$RQ.flex, df$RQ)
+
