eed39753166c12dd8af3ed366a772f5245b20a77
Chetan Srinath <chetan.srinath@gmail.com>
2018-06-04 09:35:21 -0700

    Excel Write Functions

--- R/readExcel.R
+++ R/readExcel.R
@@ -1,6 +1,7 @@
 source("R/pkgTest.R")
 pkgTest("tidyverse")
 pkgTest("readxl")
+pkgTest("openxlsx")
 
 # Makes the filepath "directory/filename". Default directory is data/
 makeFilePath <- function(fileName,directory="data") {
@@ -91,6 +92,7 @@ qPCR.file.reader <- function(df) {
   
   fullData <- data %>% 
     inner_join(unique(select(samples, Sample, Target, Biogroup)), by = c("Sample", "Target")) %>%
+    unique() %>%
     group_by(Biogroup, Target) %>% 
     mutate(Replicate = row_number()) %>%    # Number the replicates
     ungroup() %>%
@@ -98,3 +100,41 @@ qPCR.file.reader <- function(df) {
   return(fullData)
 }
 
+# Write Functions
+xls.sheet.rq <- function(df) {
+  df <- df %>% 
+    select(Target,Replicate, Biogroup, RQ) %>% 
+    spread(Biogroup,RQ) %>% 
+    select(-Replicate)
+  
+  df[is.na(df)] <- 0
+  
+  return(df)
+}
+
+xls.sheet.deltaCt <- function(df) {
+  df %>% 
+    select(Target,Replicate, Biogroup, deltaCt) %>% 
+    spread(Biogroup,deltaCt) %>% 
+    select(-Replicate) 
+}
+
+xls.sheet.Ct <- function(df) {
+  df %>% 
+    select(Target,Replicate, Biogroup, Ct) %>% 
+    spread(Biogroup,Ct) %>% 
+    select(-Replicate) 
+}
+
+qPCR.xls.write <- function(df,experiment) {
+  filename <- str_c("output/", experiment, "/", experiment, "_Results.xlsx", sep="")
+  df.mean <- summarizeData(df,RQ)
+  list("Results"=xls.sheet.rq(df),
+       "Results (avg)"=df.mean,
+       "Ct"=xls.sheet.Ct(df),
+       "Delta Ct" = xls.sheet.deltaCt(df),
+       "Raw"=df) %>%
+    write.xlsx(filename, as.table=c(TRUE,TRUE), borders="columns")
+  print(filename)
+  return(df)
+}
